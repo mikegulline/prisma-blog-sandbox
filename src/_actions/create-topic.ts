aac1460bf1bd337c7dd6b2ca130eebd12664663b
Mike Gulline <mike@mikegulline.com>
2024-01-24 17:24:34 -0800

    added create topic functionality

--- src/_actions/create-topic.ts
+++ src/_actions/create-topic.ts
@@ -1,6 +1,81 @@
 'use server';
 import { db } from '@/_db';
+import paths from '@/paths';
+import { z } from 'zod';
+import { auth } from '@/auth';
+import { redirect } from 'next/navigation';
+import { revalidatePath } from 'next/cache';
+import { Topic } from '@prisma/client';
 
-export async function createTopic() {
-  // TODO: revalidate the homepage after creating a topic
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/^[a-z-]+$/, {
+      message: 'Must be lowercase letters or dashes without spaces.',
+    }),
+  description: z.string().min(10),
+});
+
+interface CreateTopicFormState {
+  errors: {
+    name?: string[];
+    description?: string[];
+    _form?: string[];
+  };
+}
+
+export async function createTopic(
+  formState: CreateTopicFormState,
+  formData: FormData
+): Promise<CreateTopicFormState> {
+  const result = createTopicSchema.safeParse({
+    name: formData.get('name'),
+    description: formData.get('description'),
+  });
+
+  if (!result.success) {
+    return {
+      errors: result.error.flatten().fieldErrors,
+    };
+  }
+
+  const session = await auth();
+  if (!session || !session.user) {
+    return {
+      errors: {
+        _form: ['You must be signed in to create a topic.'],
+      },
+    };
+  }
+
+  // return {
+  //   errors: {},
+  // };
+
+  let topic: Topic;
+  try {
+    topic = await db.topic.create({
+      data: {
+        slug: result.data.name,
+        description: result.data.description,
+      },
+    });
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return {
+        errors: {
+          _form: [err.message],
+        },
+      };
+    } else {
+      return {
+        errors: {
+          _form: ['Something went wrong :<'],
+        },
+      };
+    }
+  }
+  revalidatePath('/');
+  redirect(paths.topichShow(topic.slug));
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+import TopicCreateForm from '@/_components/topics/topic-create-form';
+
 export default async function Home() {
-  return <main>home page</main>;
+  return (
+    <main className='grid grid-cols-4 gap-4 p-4'>
+      <div className='col-span-3'>
+        <h1 className='text-xl m-2'>Top Posts</h1>
+      </div>
+      <div>
+        <TopicCreateForm />
+      </div>
+    </main>
+  );
 }
